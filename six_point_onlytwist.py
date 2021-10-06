e38f953c5cdcd53242c9b37f99fc9227fd8a782f
hhpmbate <hhpmbate@glogin8.usr.hlrn.de>
2021-10-07 01:45:03 +0200

    Fixed the thing where for the order one result I was still taking the difference instead of the ratio

--- six_point_onlytwist.py
+++ six_point_onlytwist.py
@@ -607,8 +607,8 @@ if __name__ == "__main__":
 
     # lambdas = np.linspace(0.12,0.16,20)
     # lambdas = np.linspace(0,0.16,10)[1:]
-    # lambdas = np.linspace(0,0.04,30)
-    lambdas = np.linspace(0,0.16,30) #[1:]
+    lambdas = np.linspace(0,0.04,30)
+    # lambdas = np.linspace(0,0.16,30) #[1:]
     # lambdas = np.linspace(0,0.16,10) #[1:]
     t_range = np.arange(config["t_range0"], config["t_range1"])
     time_choice = config["time_choice"]
@@ -630,11 +630,11 @@ if __name__ == "__main__":
         matrix_1, matrix_2, matrix_3, matrix_4 = make_matrices(G2_nucl, G2_sigm, lmb_val)
 
         Gt1_1, Gt2_1 = gevp(matrix_1, time_choice, delta_t, name="_test", show=False)
-        # ratio1 = Gt1_1/Gt2_1
-        # effmass_ratio1 = stats.bs_effmass(ratio1, time_axis=1, spacing=1) / 2
-        effmassdata_1 = stats.bs_effmass(Gt1_1, time_axis=1, spacing=1)
-        effmassdata_2 = stats.bs_effmass(Gt2_1, time_axis=1, spacing=1)
-        effmass_ratio1 = np.abs(effmassdata_1 - effmassdata_2) / 2  # / lmb_val
+        ratio1 = Gt1_1/Gt2_1
+        effmass_ratio1 = stats.bs_effmass(ratio1, time_axis=1, spacing=1) / 2
+        # effmassdata_1 = stats.bs_effmass(Gt1_1, time_axis=1, spacing=1)
+        # effmassdata_2 = stats.bs_effmass(Gt2_1, time_axis=1, spacing=1)
+        # effmass_ratio1 = np.abs(effmassdata_1 - effmassdata_2) / 2  # / lmb_val
         bootfit1, redchisq1 = fit_value(effmass_ratio1, t_range)
         order0_fit[i] = bootfit1[:, 0]
         red_chisq_list[0,i] = redchisq1
