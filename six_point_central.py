2ecd0ca75a030688f32a40d0e337f740375dbb07
Mischa Batelaan <mischa.batelaan@adelaide.edu.au>
2021-11-01 00:46:07 +0100

    Added a time loop over the fit windows
    Also changed matplotlib to plt from pypl, and a few other plotting edits

--- six_point_central.py
+++ six_point_central.py
@@ -5,7 +5,7 @@ import yaml
 import sys
 import scipy.optimize as syopt
 from scipy.optimize import curve_fit
-import matplotlib.pyplot as pypl
+import matplotlib.pyplot as plt
 from matplotlib import rcParams
 
 from analysis import stats
@@ -49,7 +49,7 @@ def plotting_script_all(
     xlim = 16
     time = np.arange(0, np.shape(corr_matrix[0][0])[1])
 
-    f, axs = pypl.subplots(1, 1, figsize=(6, 6), sharex=True, sharey=True)
+    f, axs = plt.subplots(1, 1, figsize=(6, 6), sharex=True, sharey=True)
     yavg = np.average(corr_matrix[1][1], axis=0)
     ystd = np.std(corr_matrix[1][1], axis=0)
     axs.errorbar(
@@ -117,17 +117,17 @@ def plotting_script_all(
         label=r"$G_{\Sigma N}(t),\ \mathcal{O}(\lambda^1) + \mathcal{O}(\lambda^3)$",
     )
 
-    pypl.semilogy()
-    pypl.legend(fontsize="x-small")
-    # pypl.ylabel(r"$G_{nn}(t;\vec{p}=(1,0,0))$")
-    # pypl.title("$\lambda=0.04$")
-    pypl.title("$\lambda=" + str(lmb_val) + "$")
-    # pypl.xlabel(r"$\textrm{t/a}$")
-    pypl.xlabel(r"$t/a$")
-    pypl.savefig(plotdir / ("comp_plot_all_SS_" + name + ".pdf"))
+    plt.semilogy()
+    plt.legend(fontsize="x-small")
+    # plt.ylabel(r"$G_{nn}(t;\vec{p}=(1,0,0))$")
+    # plt.title("$\lambda=0.04$")
+    plt.title("$\lambda=" + str(lmb_val) + "$")
+    # plt.xlabel(r"$\textrm{t/a}$")
+    plt.xlabel(r"$t/a$")
+    plt.savefig(plotdir / ("comp_plot_all_SS_" + name + ".pdf"))
     if show:
-        pypl.show()
-    pypl.close()
+        plt.show()
+    plt.close()
     return
 
 
@@ -138,7 +138,7 @@ def plotting_script_all_N(
     xlim = 16
     time = np.arange(0, np.shape(corr_matrix[0][0])[1])
 
-    f, axs = pypl.subplots(1, 1, figsize=(6, 6), sharex=True, sharey=True)
+    f, axs = plt.subplots(1, 1, figsize=(6, 6), sharex=True, sharey=True)
     yavg = np.average(corr_matrix[0][0], axis=0)
     ystd = np.std(corr_matrix[0][0], axis=0)
     axs.errorbar(
@@ -206,17 +206,17 @@ def plotting_script_all_N(
         label=r"$G_{N\Sigma}(t),\ \mathcal{O}(\lambda^1) + \mathcal{O}(\lambda^3)$",
     )
 
-    pypl.semilogy()
-    pypl.legend(fontsize="x-small")
-    # pypl.ylabel(r"$G_{nn}(t;\vec{p}=(1,0,0))$")
-    # pypl.title("$\lambda=0.04$")
-    pypl.title("$\lambda=" + str(lmb_val) + "$")
-    # pypl.xlabel(r"$\textrm{t/a}$")
-    pypl.xlabel(r"$t/a$")
-    pypl.savefig(plotdir / ("comp_plot_all_NN_" + name + ".pdf"))
+    plt.semilogy()
+    plt.legend(fontsize="x-small")
+    # plt.ylabel(r"$G_{nn}(t;\vec{p}=(1,0,0))$")
+    # plt.title("$\lambda=0.04$")
+    plt.title("$\lambda=" + str(lmb_val) + "$")
+    # plt.xlabel(r"$\textrm{t/a}$")
+    plt.xlabel(r"$t/a$")
+    plt.savefig(plotdir / ("comp_plot_all_NN_" + name + ".pdf"))
     if show:
-        pypl.show()
-    pypl.close()
+        plt.show()
+    plt.close()
     return
 
 
@@ -227,7 +227,7 @@ def plotting_script_diff_2(
     xlim = 15
     time = np.arange(0, np.shape(diffG1)[1])
     efftime = time[:-spacing] + 0.5
-    f, axs = pypl.subplots(1, 1, figsize=(6, 6), sharex=True, sharey=True)
+    f, axs = plt.subplots(1, 1, figsize=(6, 6), sharex=True, sharey=True)
 
     yeffavg_1 = np.average(diffG1, axis=0)
     yeffstd_1 = np.std(diffG1, axis=0)
@@ -315,16 +315,16 @@ def plotting_script_diff_2(
     )
 
     axs.axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
-    # pypl.setp(axs, xlim=(0, xlim), ylim=(-0.4, 0.4))
-    pypl.setp(axs, xlim=(0, xlim), ylim=(-0.05, 0.4))
-    pypl.ylabel(r"$\Delta E_{\textrm{eff}}/\lambda$")
-    pypl.xlabel("$t/a$")
-    pypl.legend(fontsize="x-small")
-    pypl.title("$\lambda=" + str(lmb_val) + "$")
-    pypl.savefig(plotdir / ("diff_G" + name + ".pdf"))
+    # plt.setp(axs, xlim=(0, xlim), ylim=(-0.4, 0.4))
+    plt.setp(axs, xlim=(0, xlim), ylim=(-0.05, 0.4))
+    plt.ylabel(r"$\Delta E_{\textrm{eff}}/\lambda$")
+    plt.xlabel("$t/a$")
+    plt.legend(fontsize="x-small")
+    plt.title("$\lambda=" + str(lmb_val) + "$")
+    plt.savefig(plotdir / ("diff_G" + name + ".pdf"))
     if show:
-        pypl.show()
-    pypl.close()
+        plt.show()
+    plt.close()
     return
 
 def plotting_script_unpert(
@@ -347,8 +347,8 @@ def plotting_script_unpert(
     ystd_effratio = np.std(effratio, axis=0)
 
 
-    pypl.figure(figsize=(5, 5))
-    pypl.errorbar(
+    plt.figure(figsize=(5, 5))
+    plt.errorbar(
         efftime[:xlim],
         yavg_effratio[:xlim],
         ystd_effratio[:xlim],
@@ -358,14 +358,14 @@ def plotting_script_unpert(
         fmt="s",
         markerfacecolor="none",
     )
-    pypl.ylabel(r"$\textrm{eff. energy}[G_n(\mathbf{p}')/G_{\Sigma}(\mathbf{0})]$")
-    pypl.xlabel(r"$t/a$")
-    pypl.axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
-    pypl.ylim(-0.2,0.4)
-    pypl.savefig(plotdir / ("unpert_effmass.pdf"))
+    plt.ylabel(r"$\textrm{eff. energy}[G_n(\mathbf{p}')/G_{\Sigma}(\mathbf{0})]$")
+    plt.xlabel(r"$t/a$")
+    plt.axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
+    plt.ylim(-0.2,0.4)
+    plt.savefig(plotdir / ("unpert_effmass.pdf"))
 
 
-    f, axs = pypl.subplots(2, 1, figsize=(6, 6), sharex=True)
+    f, axs = plt.subplots(2, 1, figsize=(6, 6), sharex=True)
     f.tight_layout()
     axs[0].errorbar(
         efftime[:xlim],
@@ -432,24 +432,24 @@ def plotting_script_unpert(
     )
 
     # axs[0].axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
-    # pypl.setp(axs, xlim=(0, xlim), ylim=(-0.4, 0.4))
-    pypl.setp(axs, xlim=(0, xlim))
+    # plt.setp(axs, xlim=(0, xlim), ylim=(-0.4, 0.4))
+    plt.setp(axs, xlim=(0, xlim))
     axs[0].set_ylabel(r"$\textrm{Effective energy}$")
     axs[1].set_ylabel(r"$G_n(\mathbf{p}')/G_{\Sigma}(\mathbf{0})$")
-    pypl.xlabel("$t/a$")
+    plt.xlabel("$t/a$")
     axs[1].legend(fontsize="x-small")
-    # pypl.title("$\lambda=" + str(lmb_val) + "$")
-    pypl.savefig(plotdir / ("unpert_ratio" + name + ".pdf"))
+    # plt.title("$\lambda=" + str(lmb_val) + "$")
+    plt.savefig(plotdir / ("unpert_ratio" + name + ".pdf"))
     if show:
-        pypl.show()
-    pypl.close()
+        plt.show()
+    plt.close()
     return
 
 
 def plot_lmb_dep(all_data, plotdir):
     """Make a plot of the lambda dependence of the energy shift"""
-    pypl.figure(figsize=(6, 6))
-    pypl.errorbar(
+    plt.figure(figsize=(6, 6))
+    plt.errorbar(
         all_data["lambdas"],
         np.average(all_data["order0_fit"], axis=1),
         np.std(all_data["order0_fit"], axis=1),
@@ -460,7 +460,7 @@ def plot_lmb_dep(all_data, plotdir):
         elinewidth=1,
         markerfacecolor="none",
     )
-    pypl.errorbar(
+    plt.errorbar(
         all_data["lambdas"] +  0.0001,
         np.average(all_data["order1_fit"], axis=1),
         np.std(all_data["order1_fit"], axis=1),
@@ -471,7 +471,7 @@ def plot_lmb_dep(all_data, plotdir):
         elinewidth=1,
         markerfacecolor="none",
     )
-    pypl.errorbar(
+    plt.errorbar(
         all_data["lambdas"] + 0.0002,
         np.average(all_data["order2_fit"], axis=1),
         np.std(all_data["order2_fit"], axis=1),
@@ -482,7 +482,7 @@ def plot_lmb_dep(all_data, plotdir):
         elinewidth=1,
         markerfacecolor="none",
     )
-    pypl.errorbar(
+    plt.errorbar(
         all_data["lambdas"]+ 0.0003,
         np.average(all_data["order3_fit"], axis=1),
         np.std(all_data["order3_fit"], axis=1),
@@ -493,25 +493,25 @@ def plot_lmb_dep(all_data, plotdir):
         elinewidth=1,
         markerfacecolor="none",
     )
-    pypl.legend(fontsize="x-small")
-    # pypl.ylim(0, 0.2)
-    # pypl.ylim(-0.003, 0.035)
-    # pypl.xlim(-0.01, 0.22)
-    pypl.xlim(-0.01, lambdas[-1] * 1.1)
-    pypl.ylim(-0.005, np.average(all_data["order3_fit"], axis=1)[-1] * 1.3)
+    plt.legend(fontsize="x-small")
+    # plt.ylim(0, 0.2)
+    # plt.ylim(-0.003, 0.035)
+    # plt.xlim(-0.01, 0.22)
+    plt.xlim(-0.01, lambdas[-1] * 1.1)
+    plt.ylim(-0.005, np.average(all_data["order3_fit"], axis=1)[-1] * 1.3)
 
-    pypl.xlabel("$\lambda$")
-    pypl.ylabel("$\Delta E$")
-    pypl.title(rf"$t_{{0}}={all_data['time_choice']}, \Delta t={all_data['delta_t']}$")
-    pypl.axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
-    pypl.savefig(plotdir / ("lambda_dep.pdf"))
-    # pypl.show()
+    plt.xlabel("$\lambda$")
+    plt.ylabel("$\Delta E$")
+    plt.title(rf"$t_{{0}}={all_data['time_choice']}, \Delta t={all_data['delta_t']}$")
+    plt.axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
+    plt.savefig(plotdir / ("lambda_dep.pdf"))
+    # plt.show()
 
 
 def plot_lmb_dep_fit(all_data, fit_data, fitfunction, plotdir):
     """Make a plot of the lambda dependence of the energy shift"""
-    pypl.figure(figsize=(6, 6))
-    pypl.errorbar(
+    plt.figure(figsize=(6, 6))
+    plt.errorbar(
         all_data["lambdas"],
         np.average(all_data["order0_fit"], axis=1),
         np.std(all_data["order0_fit"], axis=1),
@@ -522,7 +522,7 @@ def plot_lmb_dep_fit(all_data, fit_data, fitfunction, plotdir):
         elinewidth=1,
         markerfacecolor="none",
     )
-    pypl.errorbar(
+    plt.errorbar(
         all_data["lambdas"] + 0.0001,
         np.average(all_data["order1_fit"], axis=1),
         np.std(all_data["order1_fit"], axis=1),
@@ -533,7 +533,7 @@ def plot_lmb_dep_fit(all_data, fit_data, fitfunction, plotdir):
         elinewidth=1,
         markerfacecolor="none",
     )
-    pypl.errorbar(
+    plt.errorbar(
         all_data["lambdas"]+ 0.0002,
         np.average(all_data["order2_fit"], axis=1),
         np.std(all_data["order2_fit"], axis=1),
@@ -544,7 +544,7 @@ def plot_lmb_dep_fit(all_data, fit_data, fitfunction, plotdir):
         elinewidth=1,
         markerfacecolor="none",
     )
-    pypl.errorbar(
+    plt.errorbar(
         all_data["lambdas"] +0.0003,
         np.average(all_data["order3_fit"], axis=1),
         np.std(all_data["order3_fit"], axis=1),
@@ -559,7 +559,7 @@ def plot_lmb_dep_fit(all_data, fit_data, fitfunction, plotdir):
     params = np.average(fit_data, axis=0)
     fit_ydata = fitfunction(lambdas, *params)
     print(fit_ydata)
-    pypl.plot(lambdas, fit_ydata, color="k")
+    plt.plot(lambdas, fit_ydata, color="k")
     # axs.fill_between(
     #     t_range,
     #     np.average(fitvals[1]) - np.std(fitvals[1]),
@@ -568,15 +568,15 @@ def plot_lmb_dep_fit(all_data, fit_data, fitfunction, plotdir):
     #     color=_colors[1],
     # )
 
-    pypl.legend(fontsize="x-small")
-    pypl.xlim(-0.01, 0.22)
-    pypl.ylim(0, 0.2)
-    pypl.xlabel("$\lambda$")
-    pypl.ylabel("$\Delta E$")
-    pypl.title(rf"$t_{{0}}={all_data['time_choice']}, \Delta t={all_data['delta_t']}$")
-    pypl.axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
-    pypl.savefig(plotdir / ("lambda_dep_fit.pdf"))
-    # pypl.show()
+    plt.legend(fontsize="x-small")
+    plt.xlim(-0.01, 0.22)
+    plt.ylim(0, 0.2)
+    plt.xlabel("$\lambda$")
+    plt.ylabel("$\Delta E$")
+    plt.title(rf"$t_{{0}}={all_data['time_choice']}, \Delta t={all_data['delta_t']}$")
+    plt.axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
+    plt.savefig(plotdir / ("lambda_dep_fit.pdf"))
+    # plt.show()
 
 
 def fitfunction(lmb, pars):
@@ -623,23 +623,22 @@ def fit_lmb(ydata, function, lambdas):
 
 
 def main():
-    pypl.rc("font", size=18, **{"family": "sans-serif", "serif": ["Computer Modern"]})
-    pypl.rc("text", usetex=True)
+    plt.rc("font", size=18, **{"family": "sans-serif", "serif": ["Computer Modern"]})
+    plt.rc("text", usetex=True)
     rcParams.update({"figure.autolayout": True})
 
-    pars = params(0)
+    pars = params(0) # Get the parameters for this lattice
 
     # Read in the directory data from the yaml file if one is given
     if len(sys.argv) == 2:
         config_file = sys.argv[1]
     else:
-        config_file = "data_dir.yaml"
+        config_file = "data_dir_theta2.yaml"
     print("Reading directories from: ", config_file)
     with open(config_file) as f:
         config = yaml.safe_load(f)
-    # TODO: Set up a defaults.yaml file for when there is no input file
-    pickledir = Path(config["pickle_dir1"])
-    pickledir2 = Path(config["pickle_dir2"])
+    pickledir_k1 = Path(config["pickle_dir1"])
+    pickledir_k2 = Path(config["pickle_dir2"])
     plotdir = Path(config["analysis_dir"]) / Path("plots")
     datadir = Path(config["analysis_dir"]) / Path("data")
     plotdir.mkdir(parents=True, exist_ok=True)
@@ -648,16 +647,16 @@ def main():
     # Read the correlator data from the pickle files
     mom_strings = ["p-1+0+0", "p+0+0+0", "p+1+0+0"]
     if "onlytwist" in config and config["onlytwist"]:
-        G2_nucl, G2_sigm = read_correlators2(pars, pickledir, pickledir2, mom_strings)
+        G2_nucl, G2_sigm = read_correlators2(pars, pickledir_k1, pickledir_k2, mom_strings)
     else:
-        G2_nucl, G2_sigm = read_correlators(pars, pickledir, pickledir2, mom_strings)
+        G2_nucl, G2_sigm = read_correlators(pars, pickledir_k1, pickledir_k2, mom_strings)
 
     # lambdas = np.linspace(0.006,0.013,30)
     # lambdas = np.linspace(0.008,0.012,30)
     # lambdas = np.linspace(0.005,0.011,30)
-    # lambdas = np.linspace(0,0.01,30)
+    lambdas = np.linspace(0,0.05,30)
     # lambdas = np.linspace(0,0.035,30)
-    lambdas = np.linspace(0,0.06,30)
+    # lambdas = np.linspace(0,0.06,30)
     # lambdas = np.linspace(0,0.08,30)
     # lambdas = np.linspace(0.12,0.16,20)
     # lambdas = np.linspace(0,0.16,10)[1:]
@@ -682,19 +681,20 @@ def main():
     print(aexp_function.label)
 
     # Fit to the energy gap
-    fit_range = np.arange(5,14)
-    fit_range12 = np.arange(5,18)
+    # fit_range = np.arange(5,14)
+    fit_range = np.arange(5,17)
+    fit_range12 = np.arange(5,17)
     ratio_unpert = G2_nucl[0][:, :, 0] / G2_sigm[0][:,:,0]
     bootfit1, redchisq1 = fit_value3(G2_nucl[0][:,:,0], fit_range12, aexp_function, norm=1)
     bootfit2, redchisq2 = fit_value3(G2_sigm[0][:,:,0], fit_range12, aexp_function, norm=1)
     bootfit_ratio, redchisq_ratio = fit_value(ratio_unpert, fit_range)
-    print(f"redchisq = {redchisq_ratio}")
-    print(f"fit = {np.average(bootfit1,axis=0)}")
-    print(f"fit = {np.average(bootfit2,axis=0)}")
-    print(f"fit = {np.average(bootfit_ratio,axis=0)}")
+    # print(f"redchisq = {redchisq_ratio}")
+    # print(f"fit = {np.average(bootfit1,axis=0)}")
+    # print(f"fit = {np.average(bootfit2,axis=0)}")
+    # print(f"fit = {np.average(bootfit_ratio,axis=0)}")
     diff = bootfit1[:,1]-bootfit2[:,1]
-    print(f"diff = {np.average(diff,axis=0)}")
-    print(f"diff = {err_brackets(np.average(diff),np.std(diff))}")
+    # print(f"diff = {np.average(diff,axis=0)}")
+    # print(f"diff = {err_brackets(np.average(diff),np.std(diff))}")
     plotting_script_unpert(
         G2_nucl[0][:, :, 0],
         G2_sigm[0][:,:,0],
@@ -708,9 +708,32 @@ def main():
         name="_unpert_ratio",
         show=False,
     )
-    # order0_fit[i] = bootfit1[:, 1]
-    # red_chisq_list[0, i] = redchisq1
-    
+
+    time_loop=False
+    if time_loop:
+        time_limits = [[1,20],[1,20]]
+        fitlist = stats.fit_loop(ratio_unpert, aexp_function, time_limits, plot=False, disp=True, time=False, weights_=True)
+        print(fitlist[0]["redchisq"])
+        print([i["x"] for i in fitlist])
+        print([i["chisq"] for i in fitlist])
+        print([i["redchisq"] for i in fitlist])
+        print([i["paramavg"] for i in fitlist])
+
+        with open(datadir / (f"time_window_loop.pkl"), "wb") as file_out:
+            pickle.dump(fitlist, file_out)
+            
+        lmb_val = 0.03
+        matrix_1, matrix_2, matrix_3, matrix_4 = make_matrices(
+            G2_nucl, G2_sigm, lmb_val
+        )
+        Gt1_4, Gt2_4, evals = gevp(
+            matrix_4, time_choice, delta_t, name="_test", show=False
+        )
+        ratio4 = Gt1_4 / Gt2_4
+        fitlist = stats.fit_loop(ratio4, aexp_function, time_limits, plot=False, disp=True, time=False, weights_=True)
+        with open(datadir / (f"time_window_loop_lambda.pkl"), "wb") as file_out:
+            pickle.dump(fitlist, file_out)
+
     
     for i, lmb_val in enumerate(lambdas):
         print(f"Lambda = {lmb_val}\n")
@@ -822,8 +845,8 @@ def main():
     # plot_lmb_dep(all_data, plotdir)
     # plot_lmb_dep_fit(all_data, bootfit, fitfunction2)
 
-    # pypl.figure(figsize=(6, 6))
-    # pypl.errorbar(
+    # plt.figure(figsize=(6, 6))
+    # plt.errorbar(
     #     lambdas,
     #     np.average(order0_fit, axis=1),
     #     np.std(order0_fit, axis=1),
@@ -834,7 +857,7 @@ def main():
     #     elinewidth=1,
     #     markerfacecolor="none",
     # )
-    # pypl.errorbar(
+    # plt.errorbar(
     #     lambdas+0.001,
     #     np.average(order1_fit, axis=1),
     #     np.std(order1_fit, axis=1),
@@ -845,7 +868,7 @@ def main():
     #     elinewidth=1,
     #     markerfacecolor="none",
     # )
-    # pypl.errorbar(
+    # plt.errorbar(
     #     lambdas+0.002,
     #     np.average(order2_fit, axis=1),
     #     np.std(order2_fit, axis=1),
@@ -856,7 +879,7 @@ def main():
     #     elinewidth=1,
     #     markerfacecolor="none",
     # )
-    # pypl.errorbar(
+    # plt.errorbar(
     #     lambdas+0.003,
     #     np.average(order3_fit, axis=1),
     #     np.std(order3_fit, axis=1),
@@ -867,15 +890,15 @@ def main():
     #     elinewidth=1,
     #     markerfacecolor="none",
     # )
-    # pypl.legend(fontsize="x-small")
-    # pypl.xlim(-0.01, 0.22)
-    # pypl.ylim(0, 0.2)
-    # pypl.xlabel("$\lambda$")
-    # pypl.ylabel("$\Delta E$")
-    # pypl.title(rf"$t_{{0}}={time_choice}, \Delta t={delta_t}$")
-    # pypl.axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
-    # pypl.savefig(plotdir / ("lambda_dep.pdf"))
-    # # pypl.show()
+    # plt.legend(fontsize="x-small")
+    # plt.xlim(-0.01, 0.22)
+    # plt.ylim(0, 0.2)
+    # plt.xlabel("$\lambda$")
+    # plt.ylabel("$\Delta E$")
+    # plt.title(rf"$t_{{0}}={time_choice}, \Delta t={delta_t}$")
+    # plt.axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
+    # plt.savefig(plotdir / ("lambda_dep.pdf"))
+    # # plt.show()
 
 
 if __name__ == "__main__":
