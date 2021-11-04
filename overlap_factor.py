185aef8867ebaf9754f9519cac6dc1fc8169d0e8
Mischa Batelaan <mischa.batelaan@adelaide.edu.au>
2021-11-04 04:18:53 +0100

    updated the overlap plot

--- overlap_factor.py
+++ overlap_factor.py
@@ -41,295 +41,12 @@ _markers = ["s", "o", "^", "*", "v", ">", "<", "s", "s"]
 m_N = 0.4179255
 m_S = 0.4641829
 
-
-def plotting_script_all(
-    corr_matrix, corr_matrix1, corr_matrix2, corr_matrix3, lmb_val, plotdir, name="", show=False
-):
-    spacing = 2
-    xlim = 16
-    time = np.arange(0, np.shape(corr_matrix[0][0])[1])
-
-    f, axs = plt.subplots(1, 1, figsize=(6, 6), sharex=True, sharey=True)
-    yavg = np.average(corr_matrix[1][1], axis=0)
-    ystd = np.std(corr_matrix[1][1], axis=0)
-    axs.errorbar(
-        time[:xlim],
-        yavg[:xlim],
-        ystd[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[0],
-        fmt="s",
-        markerfacecolor="none",
-        label=r"$G_{\Sigma\Sigma}(t),\ \mathcal{O}(\lambda^0)$",
-    )
-    yavg = np.average(corr_matrix1[1][1], axis=0)
-    ystd = np.std(corr_matrix1[1][1], axis=0)
-    axs.errorbar(
-        time[:xlim] + 0.2,
-        yavg[:xlim],
-        ystd[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[1],
-        fmt="s",
-        markerfacecolor="none",
-        label=r"$G_{\Sigma\Sigma}(t),\ \mathcal{O}(\lambda^0) + \mathcal{O}(\lambda^2)$",
-    )
-    yavg = np.average(corr_matrix3[1][1], axis=0)
-    ystd = np.std(corr_matrix3[1][1], axis=0)
-    axs.errorbar(
-        time[:xlim] + 0.4,
-        yavg[:xlim],
-        ystd[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[2],
-        fmt="s",
-        markerfacecolor="none",
-        label=r"$G_{\Sigma\Sigma}(t),\ \mathcal{O}(\lambda^0) + \mathcal{O}(\lambda^2) + \mathcal{O}(\lambda^4)$",
-    )
-
-    yavg = np.average(corr_matrix[1][0], axis=0)
-    ystd = np.std(corr_matrix[1][0], axis=0)
-    axs.errorbar(
-        time[:xlim],
-        yavg[:xlim],
-        ystd[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[3],
-        fmt="^",
-        markerfacecolor="none",
-        label=r"$G_{\Sigma N}(t),\ \mathcal{O}(\lambda^1)$",
-    )
-    yavg = np.average(corr_matrix2[1][0], axis=0)
-    ystd = np.std(corr_matrix2[1][0], axis=0)
-    axs.errorbar(
-        time[:xlim] + 0.2,
-        yavg[:xlim],
-        ystd[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[4],
-        fmt="^",
-        markerfacecolor="none",
-        label=r"$G_{\Sigma N}(t),\ \mathcal{O}(\lambda^1) + \mathcal{O}(\lambda^3)$",
-    )
-
-    plt.semilogy()
-    plt.legend(fontsize="x-small")
-    # plt.ylabel(r"$G_{nn}(t;\vec{p}=(1,0,0))$")
-    # plt.title("$\lambda=0.04$")
-    plt.title("$\lambda=" + str(lmb_val) + "$")
-    # plt.xlabel(r"$\textrm{t/a}$")
-    plt.xlabel(r"$t/a$")
-    plt.savefig(plotdir / ("comp_plot_all_SS_" + name + ".pdf"))
-    if show:
-        plt.show()
-    plt.close()
-    return
-
-
-def plotting_script_all_N(
-    corr_matrix, corr_matrix1, corr_matrix2, corr_matrix3, lmb_val, plotdir, name="", show=False
-):
-    spacing = 2
-    xlim = 16
-    time = np.arange(0, np.shape(corr_matrix[0][0])[1])
-
-    f, axs = plt.subplots(1, 1, figsize=(6, 6), sharex=True, sharey=True)
-    yavg = np.average(corr_matrix[0][0], axis=0)
-    ystd = np.std(corr_matrix[0][0], axis=0)
-    axs.errorbar(
-        time[:xlim],
-        yavg[:xlim],
-        ystd[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[0],
-        fmt="s",
-        markerfacecolor="none",
-        label=r"$G_{NN}(t),\ \mathcal{O}(\lambda^0)$",
-    )
-    yavg = np.average(corr_matrix1[0][0], axis=0)
-    ystd = np.std(corr_matrix1[0][0], axis=0)
-    axs.errorbar(
-        time[:xlim] + 0.2,
-        yavg[:xlim],
-        ystd[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[1],
-        fmt="s",
-        markerfacecolor="none",
-        label=r"$G_{NN}(t),\ \mathcal{O}(\lambda^0) + \mathcal{O}(\lambda^2)$",
-    )
-    yavg = np.average(corr_matrix3[0][0], axis=0)
-    ystd = np.std(corr_matrix3[0][0], axis=0)
-    axs.errorbar(
-        time[:xlim] + 0.4,
-        yavg[:xlim],
-        ystd[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[2],
-        fmt="s",
-        markerfacecolor="none",
-        label=r"$G_{NN}(t),\ \mathcal{O}(\lambda^0) + \mathcal{O}(\lambda^2) + \mathcal{O}(\lambda^4)$",
-    )
-
-    yavg = np.average(corr_matrix[0][1], axis=0)
-    ystd = np.std(corr_matrix[0][1], axis=0)
-    axs.errorbar(
-        time[:xlim],
-        yavg[:xlim],
-        ystd[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[3],
-        fmt="^",
-        markerfacecolor="none",
-        label=r"$G_{N\Sigma}(t),\ \mathcal{O}(\lambda^1)$",
-    )
-    yavg = np.average(corr_matrix2[0][1], axis=0)
-    ystd = np.std(corr_matrix2[0][1], axis=0)
-    axs.errorbar(
-        time[:xlim] + 0.2,
-        yavg[:xlim],
-        ystd[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[4],
-        fmt="^",
-        markerfacecolor="none",
-        label=r"$G_{N\Sigma}(t),\ \mathcal{O}(\lambda^1) + \mathcal{O}(\lambda^3)$",
-    )
-
-    plt.semilogy()
-    plt.legend(fontsize="x-small")
-    # plt.ylabel(r"$G_{nn}(t;\vec{p}=(1,0,0))$")
-    # plt.title("$\lambda=0.04$")
-    plt.title("$\lambda=" + str(lmb_val) + "$")
-    # plt.xlabel(r"$\textrm{t/a}$")
-    plt.xlabel(r"$t/a$")
-    plt.savefig(plotdir / ("comp_plot_all_NN_" + name + ".pdf"))
-    if show:
-        plt.show()
-    plt.close()
-    return
-
-
-def plotting_script_diff_2(
-    diffG1, diffG2, diffG3, diffG4, fitvals, t_range, lmb_val, plotdir, name="", show=False
-):
-    spacing = 2
-    xlim = 15
-    time = np.arange(0, np.shape(diffG1)[1])
-    efftime = time[:-spacing] + 0.5
-    f, axs = plt.subplots(1, 1, figsize=(6, 6), sharex=True, sharey=True)
-
-    yeffavg_1 = np.average(diffG1, axis=0)
-    yeffstd_1 = np.std(diffG1, axis=0)
-    axs.errorbar(
-        efftime[:xlim],
-        yeffavg_1[:xlim],
-        yeffstd_1[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[0],
-        fmt="s",
-        markerfacecolor="none",
-        label=r"$\mathcal{O}(\lambda^1)$",
-    )
-    axs.plot(t_range, len(t_range) * [np.average(fitvals[0])], color=_colors[0])
-    axs.fill_between(
-        t_range,
-        np.average(fitvals[0]) - np.std(fitvals[0]),
-        np.average(fitvals[0]) + np.std(fitvals[0]),
-        alpha=0.3,
-        color=_colors[0],
-    )
-    yeffavg_2 = np.average(diffG2, axis=0)
-    yeffstd_2 = np.std(diffG2, axis=0)
-    axs.errorbar(
-        efftime[:xlim] + 0.2,
-        yeffavg_2[:xlim],
-        yeffstd_2[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[1],
-        fmt="s",
-        markerfacecolor="none",
-        label=r"$\mathcal{O}(\lambda^2)$",
-    )
-    axs.plot(t_range, len(t_range) * [np.average(fitvals[1])], color=_colors[1])
-    axs.fill_between(
-        t_range,
-        np.average(fitvals[1]) - np.std(fitvals[1]),
-        np.average(fitvals[1]) + np.std(fitvals[1]),
-        alpha=0.3,
-        color=_colors[1],
-    )
-    yeffavg_3 = np.average(diffG3, axis=0)
-    yeffstd_3 = np.std(diffG3, axis=0)
-    axs.errorbar(
-        efftime[:xlim] + 0.4,
-        yeffavg_3[:xlim],
-        yeffstd_3[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[2],
-        fmt="s",
-        markerfacecolor="none",
-        label=r"$\mathcal{O}(\lambda^3)$",
-    )
-    axs.plot(t_range, len(t_range) * [np.average(fitvals[2])], color=_colors[2])
-    axs.fill_between(
-        t_range,
-        np.average(fitvals[2]) - np.std(fitvals[2]),
-        np.average(fitvals[2]) + np.std(fitvals[2]),
-        alpha=0.3,
-        color=_colors[2],
-    )
-    yeffavg_4 = np.average(diffG4, axis=0)
-    yeffstd_4 = np.std(diffG4, axis=0)
-    axs.errorbar(
-        efftime[:xlim] + 0.6,
-        yeffavg_4[:xlim],
-        yeffstd_4[:xlim],
-        capsize=4,
-        elinewidth=1,
-        color=_colors[3],
-        fmt="s",
-        markerfacecolor="none",
-        label=r"$\mathcal{O}(\lambda^4)$",
-    )
-    axs.plot(t_range, len(t_range) * [np.average(fitvals[3])], color=_colors[3])
-    axs.fill_between(
-        t_range,
-        np.average(fitvals[3]) - np.std(fitvals[3]),
-        np.average(fitvals[3]) + np.std(fitvals[3]),
-        alpha=0.3,
-        color=_colors[3],
-    )
-
-    axs.axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
-    # plt.setp(axs, xlim=(0, xlim), ylim=(-0.4, 0.4))
-    plt.setp(axs, xlim=(0, xlim), ylim=(-0.05, 0.4))
-    plt.ylabel(r"$\Delta E_{\textrm{eff}}/\lambda$")
-    plt.xlabel("$t/a$")
-    plt.legend(fontsize="x-small")
-    plt.title("$\lambda=" + str(lmb_val) + "$")
-    plt.savefig(plotdir / ("diff_G" + name + ".pdf"))
-    if show:
-        plt.show()
-    plt.close()
-    return
-
 def plotting_script_unpert(
     correlator1, correlator2, ratio, fitvals1, fitvals2, fitvals, t_range12, t_range, plotdir, name="", show=False
 ):
+    """ Plot the effective mass and ratio of the unperturbed nucleon correlators with TBC implemented in two different ways
+    """
+
     spacing = 2
     xlim = 20
     time = np.arange(0, np.shape(correlator1)[1])
@@ -346,25 +63,6 @@ def plotting_script_unpert(
     yavg_effratio = np.average(effratio, axis=0)
     ystd_effratio = np.std(effratio, axis=0)
 
-
-    # plt.figure(figsize=(5, 5))
-    # plt.errorbar(
-    #     efftime[:xlim],
-    #     yavg_effratio[:xlim],
-    #     ystd_effratio[:xlim],
-    #     capsize=4,
-    #     elinewidth=1,
-    #     color=_colors[0],
-    #     fmt="s",
-    #     markerfacecolor="none",
-    # )
-    # plt.ylabel(r"$\textrm{eff. energy}[G_n(\mathbf{p}')/G_{\Sigma}(\mathbf{0})]$")
-    # plt.xlabel(r"$t/a$")
-    # plt.axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
-    # plt.ylim(-0.2,0.4)
-    # plt.savefig(plotdir / ("unpert_effmass.pdf"))
-
-
     f, axs = plt.subplots(2, 1, figsize=(6, 6), sharex=True)
     f.tight_layout()
     axs[0].errorbar(
@@ -376,7 +74,6 @@ def plotting_script_unpert(
         color=_colors[0],
         fmt="s",
         markerfacecolor="none",
-        # label=r"$N$",
     )
     axs[0].errorbar(
         efftime[:xlim],
@@ -387,28 +84,28 @@ def plotting_script_unpert(
         color=_colors[1],
         fmt="s",
         markerfacecolor="none",
-        # label=r"$\Sigma$",
     )
-    axs[0].plot(t_range12, len(t_range12) * [np.average(fitvals1)], color=_colors[0])
+    axs[0].plot(t_range12, len(t_range12) * [np.average(fitvals1[:,1])], color=_colors[0])
     axs[0].fill_between(
         t_range12,
-        np.average(fitvals1) - np.std(fitvals1),
-        np.average(fitvals1) + np.std(fitvals1),
+        np.average(fitvals1[:,1]) - np.std(fitvals1[:,1]),
+        np.average(fitvals1[:,1]) + np.std(fitvals1[:,1]),
         alpha=0.3,
         color=_colors[0],
-        label=rf"$E_N^{{\textrm{{FermAct}}}}(\mathbf{{p}}')$ = {err_brackets(np.average(fitvals1),np.std(fitvals1))}",
+        # label=rf"$E_N^{{\textrm{{FermAct}}}}(\mathbf{{p}}')$ = {err_brackets(np.average(fitvals1[:,1]),np.std(fitvals1[:,1]))}",
+        label=rf"$E_N^{{\textrm{{FermAct}}}}(\mathbf{{p}}')$ = {err_brackets(np.average(fitvals1[:,1]),np.std(fitvals1[:,1]))}, $A_N^{{\textrm{{FermAct}}}}$ = {err_brackets(np.average(fitvals1[:,0]),np.std(fitvals1[:,0]))}",
     )
-    axs[0].plot(t_range12, len(t_range12) * [np.average(fitvals2)], color=_colors[1])
+    axs[0].plot(t_range12, len(t_range12) * [np.average(fitvals2[:,1])], color=_colors[1])
     axs[0].fill_between(
         t_range12,
-        np.average(fitvals2) - np.std(fitvals2),
-        np.average(fitvals2) + np.std(fitvals2),
+        np.average(fitvals2[:,1]) - np.std(fitvals2[:,1]),
+        np.average(fitvals2[:,1]) + np.std(fitvals2[:,1]),
         alpha=0.3,
         color=_colors[1],
-        label=rf"$E_{{N}}^{{\textrm{{Gauge}}}}(\mathbf{{p}}')$ = {err_brackets(np.average(fitvals2),np.std(fitvals2))}",
+        label=rf"$E_{{N}}^{{\textrm{{Gauge}}}}(\mathbf{{p}}')$ = {err_brackets(np.average(fitvals2[:,1]),np.std(fitvals2[:,1]))}, $A_{{N}}^{{\textrm{{Gauge}}}}$ = {err_brackets(np.average(fitvals2[:,0]),np.std(fitvals2[:,0]))}",
     )
 
-    axs[0].legend(fontsize="x-small")
+    axs[0].legend(fontsize="xx-small")
 
     axs[1].errorbar(
         time[:xlim],
@@ -435,7 +132,7 @@ def plotting_script_unpert(
     # plt.setp(axs, xlim=(0, xlim), ylim=(-0.4, 0.4))
     plt.setp(axs, xlim=(0, xlim))
     axs[0].set_ylabel(r"$\textrm{Effective energy}$")
-    axs[1].set_ylabel(r"$G_N^{\textrm{FermAct}}(\mathbf{p}')/G_{N}^{\textrm{Gauge}}(\mathbf{0})$")
+    axs[1].set_ylabel(r"$G_N^{\textrm{FermAct}}(\mathbf{p}')/G_{N}^{\textrm{Gauge}}(\mathbf{p}')$")
     plt.xlabel("$t/a$")
     axs[1].legend(fontsize="x-small")
     # plt.title("$\lambda=" + str(lmb_val) + "$")
@@ -444,183 +141,6 @@ def plotting_script_unpert(
         plt.show()
     plt.close()
     return
-
-
-def plot_lmb_dep(all_data, plotdir):
-    """Make a plot of the lambda dependence of the energy shift"""
-    plt.figure(figsize=(6, 6))
-    plt.errorbar(
-        all_data["lambdas"],
-        np.average(all_data["order0_fit"], axis=1),
-        np.std(all_data["order0_fit"], axis=1),
-        fmt="s",
-        label=r"$\mathcal{O}(\lambda^1)$",
-        color=_colors[0],
-        capsize=4,
-        elinewidth=1,
-        markerfacecolor="none",
-    )
-    plt.errorbar(
-        all_data["lambdas"] +  0.0001,
-        np.average(all_data["order1_fit"], axis=1),
-        np.std(all_data["order1_fit"], axis=1),
-        fmt="s",
-        label=r"$\mathcal{O}(\lambda^2)$",
-        color=_colors[1],
-        capsize=4,
-        elinewidth=1,
-        markerfacecolor="none",
-    )
-    plt.errorbar(
-        all_data["lambdas"] + 0.0002,
-        np.average(all_data["order2_fit"], axis=1),
-        np.std(all_data["order2_fit"], axis=1),
-        fmt="s",
-        label=r"$\mathcal{O}(\lambda^3)$",
-        color=_colors[2],
-        capsize=4,
-        elinewidth=1,
-        markerfacecolor="none",
-    )
-    plt.errorbar(
-        all_data["lambdas"]+ 0.0003,
-        np.average(all_data["order3_fit"], axis=1),
-        np.std(all_data["order3_fit"], axis=1),
-        fmt="s",
-        label=r"$\mathcal{O}(\lambda^4)$",
-        color=_colors[3],
-        capsize=4,
-        elinewidth=1,
-        markerfacecolor="none",
-    )
-    plt.legend(fontsize="x-small")
-    # plt.ylim(0, 0.2)
-    # plt.ylim(-0.003, 0.035)
-    # plt.xlim(-0.01, 0.22)
-    plt.xlim(-0.01, lambdas[-1] * 1.1)
-    plt.ylim(-0.005, np.average(all_data["order3_fit"], axis=1)[-1] * 1.3)
-
-    plt.xlabel("$\lambda$")
-    plt.ylabel("$\Delta E$")
-    plt.title(rf"$t_{{0}}={all_data['time_choice']}, \Delta t={all_data['delta_t']}$")
-    plt.axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
-    plt.savefig(plotdir / ("lambda_dep.pdf"))
-    # plt.show()
-
-
-def plot_lmb_dep_fit(all_data, fit_data, fitfunction, plotdir):
-    """Make a plot of the lambda dependence of the energy shift"""
-    plt.figure(figsize=(6, 6))
-    plt.errorbar(
-        all_data["lambdas"],
-        np.average(all_data["order0_fit"], axis=1),
-        np.std(all_data["order0_fit"], axis=1),
-        fmt="s",
-        label=r"$\mathcal{O}(\lambda^1)$",
-        color=_colors[0],
-        capsize=4,
-        elinewidth=1,
-        markerfacecolor="none",
-    )
-    plt.errorbar(
-        all_data["lambdas"] + 0.0001,
-        np.average(all_data["order1_fit"], axis=1),
-        np.std(all_data["order1_fit"], axis=1),
-        fmt="s",
-        label=r"$\mathcal{O}(\lambda^2)$",
-        color=_colors[1],
-        capsize=4,
-        elinewidth=1,
-        markerfacecolor="none",
-    )
-    plt.errorbar(
-        all_data["lambdas"]+ 0.0002,
-        np.average(all_data["order2_fit"], axis=1),
-        np.std(all_data["order2_fit"], axis=1),
-        fmt="s",
-        label=r"$\mathcal{O}(\lambda^3)$",
-        color=_colors[2],
-        capsize=4,
-        elinewidth=1,
-        markerfacecolor="none",
-    )
-    plt.errorbar(
-        all_data["lambdas"] +0.0003,
-        np.average(all_data["order3_fit"], axis=1),
-        np.std(all_data["order3_fit"], axis=1),
-        fmt="s",
-        label=r"$\mathcal{O}(\lambda^4)$",
-        color=_colors[3],
-        capsize=4,
-        elinewidth=1,
-        markerfacecolor="none",
-    )
-
-    params = np.average(fit_data, axis=0)
-    fit_ydata = fitfunction(lambdas, *params)
-    print(fit_ydata)
-    plt.plot(lambdas, fit_ydata, color="k")
-    # axs.fill_between(
-    #     t_range,
-    #     np.average(fitvals[1]) - np.std(fitvals[1]),
-    #     np.average(fitvals[1]) + np.std(fitvals[1]),
-    #     alpha=0.3,
-    #     color=_colors[1],
-    # )
-
-    plt.legend(fontsize="x-small")
-    plt.xlim(-0.01, 0.22)
-    plt.ylim(0, 0.2)
-    plt.xlabel("$\lambda$")
-    plt.ylabel("$\Delta E$")
-    plt.title(rf"$t_{{0}}={all_data['time_choice']}, \Delta t={all_data['delta_t']}$")
-    plt.axhline(y=0, color="k", alpha=0.3, linewidth=0.5)
-    plt.savefig(plotdir / ("lambda_dep_fit.pdf"))
-    # plt.show()
-
-
-def fitfunction(lmb, pars):
-    deltaE = 0.5 * (pars[0] + pars[1]) - 0.5 * np.sqrt(
-        (pars[0] - pars[1]) ** 2 + 4 * lmb ** 2 * pars[2] ** 2
-    )
-    return deltaE
-
-
-def fitfunction2(lmb, pars0, pars1, pars2):
-    deltaE = 0.5 * (pars0 + pars1) - 0.5 * np.sqrt(
-        (pars0 - pars1) ** 2 + 4 * lmb ** 2 * pars2 ** 2
-    )
-    return deltaE
-
-
-def fit_lmb(ydata, function, lambdas):
-    """Fit the lambda dependence
-
-    data is a correlator with tht bootstraps on the first index and the time on the second
-    lambdas is an array of time values to fit over
-    the function will return an array of fit parameters for each bootstrap
-    """
-    # order0_fit[i] = bootfit1[:, 0]
-    ydata = ydata.T
-    data_set = ydata
-    ydata_avg = np.average(data_set, axis=0)
-    print(ydata_avg)
-    print(lambdas)
-    covmat = np.cov(data_set.T)
-    diag_sigma = np.diag(np.std(data_set, axis=0) ** 2)
-    popt_avg, pcov_avg = curve_fit(
-        function, lambdas, ydata_avg, sigma=covmat, maxfev=2000
-    )
-    chisq = ff.chisqfn2(popt_avg, function, lambdas, ydata_avg, np.linalg.inv(covmat))
-    redchisq = chisq / len(lambdas)
-    bootfit = []
-    for iboot, values in enumerate(ydata):
-        popt, pcov = curve_fit(function, lambdas, values, sigma=covmat, maxfev=2000)
-        bootfit.append(popt)
-    bootfit = np.array(bootfit)
-
-    return bootfit, redchisq
-
 
 def main():
     """Investigate whether changing the twisted boundary conditions to the gauge field has an effect on the overlap factor of the correlator of the nucleon. 
@@ -683,13 +203,13 @@ def main():
     bootfit2, redchisq2 = fit_value3(G2_nucl2[0][:,:,0], fit_range12, aexp_function, norm=1)
     bootfit_ratio, redchisq_ratio = fit_value(ratio_unpert, fit_range)
 
-    diff = bootfit1[:,1]-bootfit2[:,1]
+    # diff = bootfit1[:,1]-bootfit2[:,1]
     plotting_script_unpert(
         G2_nucl[0][:, :, 0],
         G2_nucl2[0][:,:,0],
         ratio_unpert,
-        bootfit1[:, 1],
-        bootfit2[:, 1],
+        bootfit1, #[:, 1],
+        bootfit2, #[:, 1],
         bootfit_ratio[:, 0],
         fit_range12,
         fit_range,
