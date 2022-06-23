d1bfdcb481fc18d95ba29b4d28ae1c567ee4206e
Mischa Batelaan <mischa.batelaan@gmail.com>
2022-06-23 17:19:34 +0930

    black formatting

--- scripts/comp_fits.py
+++ scripts/comp_fits.py
@@ -40,39 +40,42 @@ m_S = 0.4641829
 
 
 def plot_fits(data_, name, plotdir):
-    tmin0_fits_ = [
-        i
-        for i in data_
-        if i["lmb_range"][0] == 0 
-    ]
+    tmin0_fits_ = [i for i in data_ if i["lmb_range"][0] == 0]
     tmin0_tmax = [i["lmb_range"][-1] for i in tmin0_fits_]
     tmin0_redchisq = [i["redchisq3"] for i in tmin0_fits_]
-    tmin1_fits_ = [
-        i
-        for i in data_
-        if i["lmb_range"][0] == 1 
-    ]
+    tmin1_fits_ = [i for i in data_ if i["lmb_range"][0] == 1]
     tmin1_tmax = [i["lmb_range"][-1] for i in tmin1_fits_]
     tmin1_redchisq = [i["redchisq3"] for i in tmin1_fits_]
-    tmin2_fits_ = [
-        i
-        for i in data_
-        if i["lmb_range"][0] == 2 
-    ]
+    tmin2_fits_ = [i for i in data_ if i["lmb_range"][0] == 2]
     tmin2_tmax = [i["lmb_range"][-1] for i in tmin2_fits_]
     tmin2_redchisq = [i["redchisq3"] for i in tmin2_fits_]
 
     plt.figure(figsize=(5, 4))
-    plt.plot(tmin0_tmax, tmin0_redchisq, color=_colors[0], label=r"$\lambda_{\textrm{min}}=0$")
-    plt.plot(tmin1_tmax, tmin1_redchisq, color=_colors[1], label=r"$\lambda_{\textrm{min}}=1$")
-    plt.plot(tmin2_tmax, tmin2_redchisq, color=_colors[2], label=r"$\lambda_{\textrm{min}}=2$")
+    plt.plot(
+        tmin0_tmax,
+        tmin0_redchisq,
+        color=_colors[0],
+        label=r"$\lambda_{\textrm{min}}=0$",
+    )
+    plt.plot(
+        tmin1_tmax,
+        tmin1_redchisq,
+        color=_colors[1],
+        label=r"$\lambda_{\textrm{min}}=1$",
+    )
+    plt.plot(
+        tmin2_tmax,
+        tmin2_redchisq,
+        color=_colors[2],
+        label=r"$\lambda_{\textrm{min}}=2$",
+    )
     plt.tight_layout()
     plt.legend(fontsize="small")
-    plt.xticks(np.arange(2,15))
+    plt.xticks(np.arange(2, 15))
     plt.xlabel(r"$\lambda_{\textrm{max}}$")
     plt.ylabel(r"$\chi^2_{\textrm{red.}}$")
     plt.grid(True, alpha=0.3)
-    plt.ylim(0,5)
+    plt.ylim(0, 5)
     plt.savefig(plotdir / (f"lambda_fit_{name}.pdf"))
     plt.close()
 
@@ -80,38 +83,53 @@ def plot_fits(data_, name, plotdir):
     tmin1_me = [i["bootfit3"][:, 1] for i in tmin1_fits_]
     tmin2_me = [i["bootfit3"][:, 1] for i in tmin2_fits_]
     plt.figure(figsize=(5, 4))
-    plt.errorbar(tmin0_tmax, np.average(tmin0_me, axis=1), np.std(tmin0_me, axis=1), color=_colors[0], label=r"$\lambda_{\textrm{min}}=0$", capsize=4, elinewidth=1, fmt=_markers[0])
-    plt.errorbar(tmin1_tmax, np.average(tmin1_me, axis=1), np.std(tmin1_me, axis=1), color=_colors[1], label=r"$\lambda_{\textrm{min}}=1$", capsize=4, elinewidth=1, fmt=_markers[1])
-    plt.errorbar(tmin2_tmax, np.average(tmin2_me, axis=1), np.std(tmin2_me, axis=1), color=_colors[2], label=r"$\lambda_{\textrm{min}}=2$", capsize=4, elinewidth=1, fmt=_markers[2])
+    plt.errorbar(
+        tmin0_tmax,
+        np.average(tmin0_me, axis=1),
+        np.std(tmin0_me, axis=1),
+        color=_colors[0],
+        label=r"$\lambda_{\textrm{min}}=0$",
+        capsize=4,
+        elinewidth=1,
+        fmt=_markers[0],
+    )
+    plt.errorbar(
+        tmin1_tmax,
+        np.average(tmin1_me, axis=1),
+        np.std(tmin1_me, axis=1),
+        color=_colors[1],
+        label=r"$\lambda_{\textrm{min}}=1$",
+        capsize=4,
+        elinewidth=1,
+        fmt=_markers[1],
+    )
+    plt.errorbar(
+        tmin2_tmax,
+        np.average(tmin2_me, axis=1),
+        np.std(tmin2_me, axis=1),
+        color=_colors[2],
+        label=r"$\lambda_{\textrm{min}}=2$",
+        capsize=4,
+        elinewidth=1,
+        fmt=_markers[2],
+    )
     plt.tight_layout()
     plt.legend(fontsize="small")
-    plt.xticks(np.arange(2,15))
+    plt.xticks(np.arange(2, 15))
     plt.xlabel(r"$\lambda_{\textrm{max}}$")
     plt.ylabel(r"Matrix element")
     plt.grid(True, alpha=0.3)
     # plt.ylim(0,5)
-    plt.savefig(plotdir / (f"lambda_fit_"+name+"_me.pdf"))
+    plt.savefig(plotdir / (f"lambda_fit_" + name + "_me.pdf"))
     plt.close()
 
     return
 
 
 def plot_fits_comb(data_, name, plotdir):
-    lmbmin0_fits_ = [
-        i
-        for i in data_
-        if i["lmb_range"][0] == 0 
-    ]
-    lmbmin1_fits_ = [
-        i
-        for i in data_
-        if i["lmb_range"][0] == 1 
-    ]
-    lmbmin2_fits_ = [
-        i
-        for i in data_
-        if i["lmb_range"][0] == 2 
-    ]
+    lmbmin0_fits_ = [i for i in data_ if i["lmb_range"][0] == 0]
+    lmbmin1_fits_ = [i for i in data_ if i["lmb_range"][0] == 1]
+    lmbmin2_fits_ = [i for i in data_ if i["lmb_range"][0] == 2]
     lmbmin0_lmbmax = [i["lmb_range"][-1] for i in lmbmin0_fits_]
     lmbmin1_lmbmax = [i["lmb_range"][-1] for i in lmbmin1_fits_]
     lmbmin2_lmbmax = [i["lmb_range"][-1] for i in lmbmin2_fits_]
@@ -126,11 +144,26 @@ def plot_fits_comb(data_, name, plotdir):
     lmbmin2_me = [i["bootfit3"][:, 1] for i in lmbmin2_fits_]
 
     fig, ax1 = plt.subplots(figsize=(7, 5))
-    ax1.plot(lmbmin0_lmbmax_, lmbmin0_redchisq, color=_colors[0], label=rf"$\lambda_{{\textrm{{min}}}}={lmbmin0_fits_[0]['lambdas3'][0]:.3f}$")
-    ax1.plot(lmbmin1_lmbmax_, lmbmin1_redchisq, color=_colors[1], label=rf"$\lambda_{{\textrm{{min}}}}={lmbmin1_fits_[0]['lambdas3'][0]:.3f}$")
-    ax1.plot(lmbmin2_lmbmax_, lmbmin2_redchisq, color=_colors[2], label=rf"$\lambda_{{\textrm{{min}}}}={lmbmin2_fits_[0]['lambdas3'][0]:.3f}$")
+    ax1.plot(
+        lmbmin0_lmbmax_,
+        lmbmin0_redchisq,
+        color=_colors[0],
+        label=rf"$\lambda_{{\textrm{{min}}}}={lmbmin0_fits_[0]['lambdas3'][0]:.3f}$",
+    )
+    ax1.plot(
+        lmbmin1_lmbmax_,
+        lmbmin1_redchisq,
+        color=_colors[1],
+        label=rf"$\lambda_{{\textrm{{min}}}}={lmbmin1_fits_[0]['lambdas3'][0]:.3f}$",
+    )
+    ax1.plot(
+        lmbmin2_lmbmax_,
+        lmbmin2_redchisq,
+        color=_colors[2],
+        label=rf"$\lambda_{{\textrm{{min}}}}={lmbmin2_fits_[0]['lambdas3'][0]:.3f}$",
+    )
     ax1.set_ylabel(r"$\chi^2_{\textrm{red.}}$")
-    ax1.set_ylim(0,5)
+    ax1.set_ylim(0, 5)
     ax1.grid(True, alpha=0.3)
     # ax1.set_xticks(np.arange(2,15))
     # ax1.set_xticks()
@@ -140,22 +173,49 @@ def plot_fits_comb(data_, name, plotdir):
     # print(f"lmbmin0_me = {np.average(lmbmin0_me, axis=1)}")
 
     ax2 = ax1.twinx()
-    ax2.errorbar(lmbmin0_lmbmax_, np.average(lmbmin0_me, axis=1), np.std(lmbmin0_me, axis=1), color=_colors[0], capsize=4, elinewidth=1, fmt=_markers[0], label=rf"$\lambda_{{\textrm{{min}}}}={lmbmin0_fits_[0]['lambdas3'][0]:.3f}$")
-    ax2.errorbar(lmbmin1_lmbmax_, np.average(lmbmin1_me, axis=1), np.std(lmbmin1_me, axis=1), color=_colors[1], capsize=4, elinewidth=1, fmt=_markers[1], label=rf"$\lambda_{{\textrm{{min}}}}={lmbmin1_fits_[0]['lambdas3'][0]:.3f}$")
-    ax2.errorbar(lmbmin2_lmbmax_, np.average(lmbmin2_me, axis=1), np.std(lmbmin2_me, axis=1), color=_colors[2], capsize=4, elinewidth=1, fmt=_markers[2], label=rf"$\lambda_{{\textrm{{min}}}}={lmbmin2_fits_[0]['lambdas3'][0]:.3f}$")
+    ax2.errorbar(
+        lmbmin0_lmbmax_,
+        np.average(lmbmin0_me, axis=1),
+        np.std(lmbmin0_me, axis=1),
+        color=_colors[0],
+        capsize=4,
+        elinewidth=1,
+        fmt=_markers[0],
+        label=rf"$\lambda_{{\textrm{{min}}}}={lmbmin0_fits_[0]['lambdas3'][0]:.3f}$",
+    )
+    ax2.errorbar(
+        lmbmin1_lmbmax_,
+        np.average(lmbmin1_me, axis=1),
+        np.std(lmbmin1_me, axis=1),
+        color=_colors[1],
+        capsize=4,
+        elinewidth=1,
+        fmt=_markers[1],
+        label=rf"$\lambda_{{\textrm{{min}}}}={lmbmin1_fits_[0]['lambdas3'][0]:.3f}$",
+    )
+    ax2.errorbar(
+        lmbmin2_lmbmax_,
+        np.average(lmbmin2_me, axis=1),
+        np.std(lmbmin2_me, axis=1),
+        color=_colors[2],
+        capsize=4,
+        elinewidth=1,
+        fmt=_markers[2],
+        label=rf"$\lambda_{{\textrm{{min}}}}={lmbmin2_fits_[0]['lambdas3'][0]:.3f}$",
+    )
     ax2.set_ylabel(r"Matrix element")
 
     ax1.legend(fontsize="small")
     ax2.legend(fontsize="small")
 
-    plt.savefig(plotdir / (f"lambda_fit_"+name+"_comb.pdf"))
+    plt.savefig(plotdir / (f"lambda_fit_" + name + "_comb.pdf"))
     plt.close()
 
     return
 
 
 def main():
-    """ Fit to the lambda dependence of the energy shift and loop over the fit windows """
+    """Fit to the lambda dependence of the energy shift and loop over the fit windows"""
     mystyle = Path(PROJECT_BASE_DIRECTORY) / Path("gevpanalysis/mystyle.txt")
     plt.style.use(mystyle.as_posix())
 
@@ -203,12 +263,12 @@ def main():
     for i, elem in enumerate(data_4pts_fn4):
         print(elem["lmb_range"], "\t\t", elem["redchisq3"])
 
-
     # plot_fits(data_3pts, "3points", plotdir)
     # plot_fits(data_4pts, "4points", plotdir)
     plot_fits_comb(data_3pts, "3points", plotdir)
     plot_fits_comb(data_4pts, "4points", plotdir)
     plot_fits_comb(data_4pts_fn4, "4points_fn4", plotdir)
+
 
 if __name__ == "__main__":
     main()
